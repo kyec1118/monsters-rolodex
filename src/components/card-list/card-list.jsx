119e6e0f5469b4d303980775ef92b2c79854a8c7
Kuyee Chang <kyec1118@gmail.com>
2021-08-16 20:36:10 +0800

    initial commit

---
+++ src/components/card-list/card-list.jsx
@@ -0,0 +1,13 @@
+import "./card-list.css";
+import Card from "../card/card";
+
+const CardList = (props) => {
+	return (
+		<div className="card-list">
+			{props.monsters.map((monster) => (
+				<Card key={monster.id} monster={monster} />
+			))}
+		</div>
+	);
+};
+export default CardList;
